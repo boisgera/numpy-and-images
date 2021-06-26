a4d2cc88f3fba7a0c57f02c932ab7226e993d293
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-06-26 11:33:22 +0200

    basic notebook

--- notebook.py
+++ notebook.py
@@ -1,2 +1,14 @@
 # Jupyter Notebook (percent format)
 
+# %%
+from numpy import *
+from PIL import Image
+
+# %%
+path = "images/mike-dorner-sf_1ZDA1YFw-unsplash.jpg"
+image = Image.open(path)
+im_array = array(image)
+print(im_array)
+
+# %%
+Image.fromarray(im_array)
